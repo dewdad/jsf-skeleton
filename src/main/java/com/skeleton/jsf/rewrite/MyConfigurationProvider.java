3e5c59acadbd003a5ae7238a9c7aad40ed70f49b
NaTsuThery <thery.charasson@gmail.com>
2014-07-06 13:23:03 +0200

    add slf4j + logback configuration
    add example in MyConfigurationProvider
    Lombok allow you to use @Slf4j instead of 
    final static Logger logger = LoggerFactory.getLogger(MyApp1.class);

--- src/main/java/com/skeleton/jsf/rewrite/MyConfigurationProvider.java
+++ src/main/java/com/skeleton/jsf/rewrite/MyConfigurationProvider.java
@@ -2,6 +2,8 @@ package com.skeleton.jsf.rewrite;
 
 import javax.servlet.ServletContext;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
@@ -19,11 +21,12 @@ import org.ocpsoft.rewrite.servlet.config.rule.Join;
  *
  */
 @RewriteConfiguration
+@Slf4j
 public class MyConfigurationProvider extends HttpConfigurationProvider {
 
   @Override
   public Configuration getConfiguration(ServletContext context) {
-
+    log.info("Initialize Rewrite Configuration");
     // Add index rule
     Rule indexRule = Join.path("/index").to("faces/pages/index.xhtml");
     return ConfigurationBuilder.begin().addRule(indexRule);
